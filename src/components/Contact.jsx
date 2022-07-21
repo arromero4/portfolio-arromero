cce05187f525368f32a149fc72c866335f9d9872
arromero4 <arromero0408@gmail.com>
2022-07-21 03:06:48 -0500

    arromero portfolio - construction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,10 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#C53634]'>Contact</p>
                 <p className='text-gray-300 py-4'>Submit the form below or shoot me an email - arromero0408@gmail.com</p>
             </div>
-
+            <div class="relative">
+              <input type="text" id="floating_outlined" className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+              <label for="floating_outlined" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Floating outlined</label>
+          </div>
             <input className='my-2 p-2 bg-[#eaefff]' type="text" placeholder='Name' name="name"/>
             <input className='my-2 p-2 bg-[#eaefff]' type="email" placeholder='Email' name="email"/>
             <input className='my-2 p-2 bg-[#eaefff]' type="text" placeholder='Subject' name="subject"/>
